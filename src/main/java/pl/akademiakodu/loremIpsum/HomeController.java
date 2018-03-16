842b8baa67c6bd687160c2522042fadac6230621
Wiola <w.rudzka1@gmail.com>
2018-03-16 15:33:23 +0100

    create form.html

--- src/main/java/pl/akademiakodu/loremIpsum/HomeController.java
+++ src/main/java/pl/akademiakodu/loremIpsum/HomeController.java
@@ -1,4 +1,19 @@
 package pl.akademiakodu.loremIpsum;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+
+    @GetMapping("/")
+    public String form() {
+        return "form";
+    }
+
+    @GetMapping("/print")
+    public String print() {
+        return "print";
+    }
 }
