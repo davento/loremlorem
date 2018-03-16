cfd25311698da39b6ee39d55648196c9822fe408
Dawid Borcuch <dawid.borcuch@gmail.com>
2018-03-16 15:09:23 +0100

    second commit

--- src/main/java/pl/akademiakodu/loremIpsum/HomeController.java
+++ src/main/java/pl/akademiakodu/loremIpsum/HomeController.java
@@ -1,4 +1,14 @@
 package pl.akademiakodu.loremIpsum;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/loremIpsum")
+    public String getLoremIpsum() {
+        return "loremIpsum";
+    }
+
 }
---
+++ src/main/java/pl/akademiakodu/loremIpsum/LoremIpsumGenerator.java
@@ -0,0 +1,6 @@
+package pl.akademiakodu.loremIpsum;
+
+public interface LoremIpsumGenerator extends {
+
+
+}
